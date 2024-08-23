9e13068d9b7ce7cf326800a807aeccecc8a5ee6c
Vaji <130310621+VajiheMobasheri@users.noreply.github.com>
2024-08-23 13:32:19 +0330

    The codes have been added.

---
+++ famouspeople.py
@@ -0,0 +1,52 @@
+famous_people_list = [
+    {
+        "famous person name": "Cristiano Ronaldo",
+        "number of followers on Instagram": 602_000_000,
+        "where they are from": "Portugal"
+    },
+    {
+        "famous person name": "Lionel Messi",
+        "number of followers on Instagram": 487_000_000,
+        "where they are from": "Argentina"
+    },
+    {
+        "famous person name": "Selena Gomez",
+        "number of followers on Instagram": 430_000_000,
+        "where they are from": "United States"
+    },
+    {
+        "famous person name": "Dwayne 'The Rock' Johnson",
+        "number of followers on Instagram": 400_000_000,
+        "where they are from": "United States"
+    },
+    {
+        "famous person name": "Ariana Grande",
+        "number of followers on Instagram": 390_000_000,
+        "where they are from": "United States"
+    },
+    {
+        "famous person name": "Kylie Jenner",
+        "number of followers on Instagram": 398_000_000,
+        "where they are from": "United States"
+    },
+    {
+        "famous person name": "Kim Kardashian",
+        "number of followers on Instagram": 364_000_000,
+        "where they are from": "United States"
+    },
+    {
+        "famous person name": "Beyoncé",
+        "number of followers on Instagram": 320_000_000,
+        "where they are from": "United States"
+    },
+    {
+        "famous person name": "Taylor Swift",
+        "number of followers on Instagram": 275_000_000,
+        "where they are from": "United States"
+    },
+    {
+        "famous person name": "Neymar Jr.",
+        "number of followers on Instagram": 225_000_000,
+        "where they are from": "Brazil"
+    }
+]
---
+++ main.py
@@ -0,0 +1,41 @@
+from famouspeople import famous_people_list
+import random
+
+
+#Define a function to randomly select two celebrities from the list.
+def random_name(famous_people):
+    random_person_a = random.choice(famous_people)
+    random_person_b = random.choice(famous_people)
+    return random_person_a, random_person_b
+
+
+#Define a function to calculate the user's score.
+def calculate_score(random_person_a, random_person_b, guess):
+    if (guess == "A" and random_person_a['number of followers on Instagram'] > random_person_b[
+        'number of followers on Instagram']) or (
+            guess == "B" and random_person_a['number of followers on Instagram'] > random_person_b[
+        'number of followers on Instagram']):
+        return True
+    else:
+        return False
+
+
+def game():
+    score = 0
+    while True:
+        random_person_a, random_person_b = random_name(famous_people_list)
+        #Display information about the two celebrities to the user.
+        print(f"Compare A : {random_person_a['famous person name']}, {random_person_a['where they are from']}")
+        print(f"Compare B : {random_person_b['famous person name']}, {random_person_b['where they are from']}")
+        #Ask for the user's guess.
+        guess = input("Who has more followers? A or B?")
+        #Check the user's guess.
+        if calculate_score(random_person_a, random_person_b, guess):
+            score += 1
+            print(f"That's right! your score is : {score}")
+        else:
+            print(f"You lose! your score is : {score}")
+            break
+
+
+game()
